f3d5dd4166e3e4f25d59a2bc9ea3b162b8b20793
Soul Lee <me@soulee.dev>
2026-01-19 21:08:18 +0900

    fix: 파일 확장자명 변경

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,12 @@
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-blog'
 import { MDXComponents } from 'nextra/mdx-components'
+import ArticleLayout from '@/components/ArticleLayout'
 
 const themeComponents = getThemeComponents()
- 
+
 export function useMDXComponents(components?: MDXComponents) {
   return {
     ...themeComponents,
-    ...components
+    ...components,
   }
 }
