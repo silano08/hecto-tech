a784f8d44f9f3e18b25744d0d528c3b0741f9b7f
Soul Lee <me@soulee.dev>
2026-01-19 20:38:06 +0900

    feat: Nextra 기본 설정

---
+++ app/[[...mdxPath]]/page.tsx
@@ -0,0 +1,32 @@
+import { Fragment } from 'react'
+import { generateStaticParamsFor, importPage } from 'nextra/pages'
+import { useMDXComponents as getMDXComponents } from '@/mdx-components'
+
+type Props = {
+  params: Promise<{ mdxPath?: string[] }>
+}
+
+export const generateStaticParams = generateStaticParamsFor('mdxPath')
+
+export async function generateMetadata(props: Props) {
+  const params = await props.params
+  const { metadata } = await importPage(params.mdxPath)
+  return metadata
+}
+ 
+const Wrapper = getMDXComponents().wrapper ?? Fragment
+ 
+export default async function Page(props: Props) {
+  const params = await props.params
+  const {
+    default: MDXContent,
+    toc,
+    metadata,
+    sourceCode
+  } = await importPage(params.mdxPath)
+  return (
+    <Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
+      <MDXContent {...props} params={params} />
+    </Wrapper>
+  )
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import nextra from "nextra";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withNextra = nextra({
 
-export default nextConfig;
+})
+
+export default withNextra({
+  
+})
